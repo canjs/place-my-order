7a58be88572b55bcdd49deac00cf9727de61f69c
Chasen Le Hara <chasen@chasenlehara.com>
2015-06-03 17:18:55 -0700

    Combine app.js and routes.js files
    Related to #2

--- app/routes.js
+++
@@ -1,7 +0,0 @@
-can.route(':page', { page: 'home' });
-can.route(':page/:slug', { slug: null });
-can.route(':page/:slug/:action', { slug: null, action: null });
-
-$('body').on('click', 'a[href="javascript://"]', function(ev) {
-  ev.preventDefault();
-});
